8c97eb6182a49062533172458af5db6c8c1647ba
asullivan <sullivan1513@gmail.com>
2021-11-06 02:49:30 -0400

    All the things

--- client/src/index.js
+++ client/src/index.js
@@ -11,6 +11,10 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+function iAmTheBackend() {
+  console.log("I am printing on the terminal.");
+}
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
